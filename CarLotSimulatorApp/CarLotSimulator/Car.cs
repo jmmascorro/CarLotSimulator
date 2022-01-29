b5f7e381f2e7cc0fa312509b1ab960c1e43bed47
jmmascorro <jessemascorro88@gmail.com>
2022-01-29 11:59:56 -0600

    finsihed ClassesExercise2

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarLotSimulator
+{
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string EngineNoise { get; set; }
+        public string HonkNoise { get; set; }
+        public bool IsDrivable { get; set; }
+        
+
+        public Car()
+        {
+
+        }
+
+        public Car(int year, string make, string model, string enginenoise, string honknoise, bool isDrivable = false )
+        {
+            Year = year;
+            Make = make;
+            Model = model;
+            EngineNoise = enginenoise;
+            HonkNoise = honknoise;
+        }
+
+        public void MakeEngineNoise(string engineNoise)
+        {
+            Console.WriteLine(engineNoise);
+        }
+
+        public void MakeHonkNoise()
+        {
+            Console.WriteLine(HonkNoise);
+        }
+            
+          
+
+    }
+}
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -21,13 +21,53 @@ namespace CarLotSimulator
             //*************BONUS*************//
 
             // Set the properties utilizing the 3 different ways we learned about, one way for each car
+            Carlot lot = new Carlot();
+            
+      
+            //Standard member initializer syntax
+            Car chevy = new Car();
+            lot.CarList.Add(chevy);
+
+            chevy.Year = 2004;
+            chevy.Make = "Chevrolet";
+            chevy.Model = "Silverado";
+            chevy.EngineNoise = "Vroom";
+            chevy.HonkNoise = "Beep beep";
+            chevy.IsDrivable = true;
+
+            chevy.MakeEngineNoise(chevy.EngineNoise);
+            chevy.MakeHonkNoise();
+
+            //Object initializer syntax
+            Car jeep = new Car() { Year = 2015, Make = "Jeep", Model = "Grand Cherokee", EngineNoise = "Vroom", HonkNoise = "BEEP BEEP", IsDrivable = true };
+            lot.CarList.Add(jeep);
+
+            jeep.MakeEngineNoise(jeep.EngineNoise);
+            jeep.MakeHonkNoise();
+
+            //Constructor initialization
+            Car suv = new Car(2018, "Ford", "Escape", "vroom", "beep");
+            lot.CarList.Add(suv);
+
+            suv.MakeEngineNoise(suv.EngineNoise);
+            suv.MakeHonkNoise();
+
+            Console.WriteLine("--------------------");
 
             //*************BONUS X 2*************//
+
+            foreach(var car in lot.CarList)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Year: {car.Year} Make: {car.Make} Model: {car.Model}");
+
+            }
 
             //Create a CarLot class
             //It should have at least one property: a List of cars
             //Instanciate the a Carlot at the beginning of the program and as you create a car add the car to the list.
             //At the end iterate through the list printing each of car's Year, Make, and Model to the console
+
         }
     }
 }
